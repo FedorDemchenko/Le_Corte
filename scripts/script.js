ff43d640b947d355d9d480c2a966c86e3f2d2c19
FedorDemchenko <111728554+FedorDemchenko@users.noreply.github.com>
2022-08-29 23:30:38 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
+const bigFoto = document.querySelector('.slider>.slider__foto>img');
+const smallFoto = document.getElementsByClassName('slider_thumbnail');
 
+for (let foto of smallFoto) {
+  foto.addEventListener('click', changeFoto);
+}
+
+function changeFoto(){
+  let bigPhoto = bigFoto.src;
+  bigFoto.src = this.src;
+  this.src = bigPhoto;
+}
